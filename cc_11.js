c1b8bf6366a8c60f3c4b2cc16d4c3d583e9b0c0a
Lisbeth Arias Cabral <lariascabral@usf.edu>
2025-03-01 15:52:27 -0500

    Task 3 - Created Library Class

--- cc_11.js
+++ cc_11.js
@@ -58,3 +58,31 @@ console.log(borrower1.borrowedBooks);
 borrower1.returnBook("The Great Gatsby");
 console.log(borrower1.borrowedBooks);
 // Expected output: []
+
+
+// Task 3 - Created Library Class
+
+class Library {
+    constructor() {
+        this.books = [],                            // Creating of books array instances
+        this.borrowers  = []                        // Creation of borrowers array
+    } ;                                             // Creating Library class
+
+    addBook(book) {                                 // Method to add new books into the Library class
+        this.books.push(book);                      // Each new book will be push to the end of the list
+    } ;
+
+    listBooks() {                                   // Listing method of all books in the console separately using forEach
+     this.books.forEach(book => {
+        console.log(`Title: ${book.title}, Author: ${book.author}, ISBN: ${book.isbn}, Copies: ${book.copies}`) ;
+      })                                            // Each book will be listed with all the above properties
+    }
+}
+
+// Test Data
+const library = new Library();
+library.addBook(book1);
+library.listBooks();
+// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
+
+
